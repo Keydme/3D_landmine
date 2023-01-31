3b53b4f3e1f2232bb4c476bccfe2accefeebb558
Keydme <87051268+Keydme@users.noreply.github.com>
2023-01-31 09:39:09 +0800

    first commit

--- main.cpp
+++ main.cpp
@@ -1,36 +1,95 @@
 #include <iostream>
 #include <random.h>
-/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
 using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-
-int test();
-struct Tripoint{
-	int x,y,z;
-	bool flag;
-	bool visi;
-};
-int a,b,c;
+void change_1(int *a);
+void change_2(int *a);
+void print_1(int *a);
+int check_1(int *a);
+int check_2(int*a, int *b, int n);
+int lmn;
+int total;
 
 int main(int argc, char** argv) {
-	setRandomSeed(1);
-	cin>>a>>b>>c;
-	int sum = a*b*c;
-	int const *n = &sum;
-	Tripoint l[sum];
-	for (int i=0;i<sum;i++)
-		cout<<l[i].x<<endl;
-	cout<<sizeof(l)<<endl;
-	cout<<sizeof(int)<<endl;
+//	setRandomSeed(1);
+
+	int x,y,z;
+	int a,b,c;
+	cin>>x>>y>>z;
+	cin>>lmn;
+	total = x*y*z;
+	const int *n = &total;
+	cout<<x<<"\t"<<y<<"\t"<<z<<"\t"<<*n<<endl;
+	int d[*n]={0};
+	cout<<"abc  "<<sizeof(d)/sizeof(d[0])<<endl;
+	
+	cout<<r[0]<<endl;
+	
+//	d[total-1]=-1;
+	print_1(d);
+	change_1(d);
+	print_1(d);
+//	change_2(d);
+	print_1(d);
+	while (true){
+		cin>>a>>b>>c;
+		cout<<a;
+		if (a==1) break;
+	}
 	return 0;
 }
 
-int test(){
-	return randomInteger(1,10);
-}
-
-void init(Tripoint *l){
-	for(int i=0;i<sizeof(l)/sizeof(l[0]);i++){
-		
+void change_1(int *a){
+	int t,times=1;
+	while(true){
+		cout<<times<<"\n\n\n\n";
+		times++;
+		t=randomInteger(0, total);
+//		cout<<"hdsjhdjsdhjd\t"<<t<<"\t"<<total<<endl;
+		a[t]=-1;
+		//print_1(a);
+		if (check_1(a)==lmn) break;
 	}
 }
+
+void change_2(int *a){
+	for (int i=0;i<total;i++){
+		if (a[i]==-1) ;
+		else a[i]=check_2(a, r, i);
+	}
+}
+
+int check_1(int *a){
+	int n=0;
+	for (int i=0;i<total;i++){
+//		cout<<"\n"<<n<<endl;
+		if(a[i]==-1){
+			n++;
+		}
+	}
+	return n;
+}
+
+int check_2(int*a, int *b, int n){
+	int times=0;
+	for(int i=n-13;i<n+13+1;i++)
+		if (a[i]==-1 && i>=0 && i<total)
+			times++;
+	return times;
+}
+
+void print_1(int *a){
+	for (int i=0;i<total;i++)
+		cout<<a[i]<<"\t";
+	cout<<endl;
+}
+
+void print_2(int *a, int *b){
+	
+}
+
+
+
+
--- random.cpp
+++ random.cpp
@@ -6,7 +6,7 @@
 
 /*************************************************************************/
 /* Stanford Portable Library                                             */
-/* Copyright (C) 2013 by Eric Roberts <eroberts@cs.stanford.edu>         */
+/* Copyright (c) 2014 by Eric Roberts <eroberts@cs.stanford.edu>         */
 /*                                                                       */
 /* This program is free software: you can redistribute it and/or modify  */
 /* it under the terms of the GNU General Public License as published by  */
